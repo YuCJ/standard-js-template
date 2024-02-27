9eebd76af25387c1a2a034228a165b8d1a51578e
yucj <nakts0123@gmail.com>
2024-02-27 17:15:38 +0800

    chore: remove tests

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
-const isTest = process.env.NODE_ENV === 'test'
-
 module.exports = {
-  ignore: isTest ? [] : ['**/__test__/**/*'],
   presets: ['@babel/env'],
   plugins: ['@babel/plugin-proposal-class-properties'],
 }
